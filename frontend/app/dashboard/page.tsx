684d71c2123a6d6913fe758b5aff1a7008ede281
Hridoy Molla <qualixe.hridoy@gmail.com>
2026-09-10 11:36:04 +0600

    dashboard design revert

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -154,7 +154,7 @@ export default async function DashboardPage() {
 
       <StatsSectionCard title="Customers" stats={customerStats} show_title={false} />
       <StatsSectionCard title="Campaigns & Gifts" stats={campaignsAndGifts} show_title={false} />
-      {/* <StatsSectionCard title="Birthdays" stats={birthdayStatDefs} show_title={false} /> */}
+      <StatsSectionCard title="Birthdays" stats={birthdayStatDefs} show_title={false} />
 
       <div className="grid gap-4 lg:grid-cols-3 items-stretch">
         <div className="lg:col-span-2 flex flex-col">
